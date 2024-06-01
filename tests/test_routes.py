c53ff8d4dc787986eac1d539e04942445fcdbfe3
Hendrik121 <hendrik121@hotmail.de>
2024-06-01 17:16:01 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -199,6 +199,19 @@ class TestProductRoutes(TestCase):
         updated_product = response.get_json()
         self.assertEqual(updated_product["description"], "unknown")
 
+    def test_update_product_not_found(self):
+        """It should not update an Product without id"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the product
+        new_product = response.get_json()
+        new_product["id"] = 0
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_delete_product(self):
         """It should Delete a Product"""
         products = self._create_products(5)
